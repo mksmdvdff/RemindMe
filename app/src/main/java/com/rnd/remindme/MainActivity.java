96f8c0c5df5059509e86ce23028382bc4cf3b2bd
mksmdvdff <mksmdavydoff@gmail.com>
2015-12-16 22:42:18 +0300

    second lesson

---
+++ app/src/main/java/com/rnd/remindme/MainActivity.java
@@ -0,0 +1,16 @@
+package com.rnd.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Техно on 16.12.2015.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
